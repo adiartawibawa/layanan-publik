796d53d5389effa09af8133c432ed618dc88d5c3
Adi Arta Wibawa <surat.buat.adi@gmail.com>
2023-09-07 18:24:38 +0800

    add qrcode

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -184,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
     ])->toArray(),
 
 ];
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,33 +1,41 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">{{ __('Dashboard') }}</h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <section class="mb-8">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <section class="mb-8 flex flex-col md:flex-row gap-4">
                 <div class="w-2/3">
                     <x-card title="Permohonan Saya">
                         @forelse ($permohonans as $item)
                             <div class="mb-12">
                                 <div class="w-full">
                                     <div class="flex flex-row gap-2">
-                                        <img class="h-32 w-auto"
-                                            src="https://upload.wikimedia.org/wikipedia/commons/3/31/MM_QRcode.png"
-                                            alt="Kode Permohonan" srcset="">
-                                        <div class="w-full flex flex-col max-h-fulll justify-between">
-                                            <div class="text-xl font-semibold text-gray-800 mb-2">
+                                        {{--
+                                    <img
+                                        class="h-32 w-auto"
+                                        src="https://upload.wikimedia.org/wikipedia/commons/3/31/MM_QRcode.png"
+                                        alt="Kode Permohonan"
+                                        srcset=""
+                                    />
+                                    --}}
+                                        <div class="h-32 w-32 object-cover object-center">
+                                            {!! QrCode::size(128)->style('round')->generate($item->kode_mohon) !!}
+                                        </div>
+                                        <div class="max-h-fulll flex w-full flex-col justify-between">
+                                            <div class="mb-2 text-xl font-semibold text-gray-800">
                                                 {{ $item->layanan_name }}
                                             </div>
-                                            <div class="text-sm text-gray-600 mb-1">Kode Permohonan :
+                                            <div class="mb-1 text-sm text-gray-600">
+                                                Kode Permohonan :
                                                 <span class="font-semibold">{{ $item->kode_mohon }}</span>
                                             </div>
-                                            <div class="text-sm text-gray-600 mb-1">Dibuat :
-                                                {{ $item->created_at->diffForHumans() }}</div>
-                                            <div class="mb-auto text-sm text-gray-600">Status Terakhir :
-                                                @switch($item->status_aktivitas)
+                                            <div class="mb-1 text-sm text-gray-600">
+                                                Dibuat : {{ $item->created_at->diffForHumans() }}
+                                            </div>
+                                            <div class="mb-auto text-sm text-gray-600">
+                                                Status Terakhir : @switch($item->status_aktivitas)
                                                     @case(1)
                                                         <span>Permohonan telah diterima</span>
                                                     @break
@@ -48,35 +56,42 @@
                                                         <span>Permohonan baru dibuat</span>
                                                 @endswitch
                                             </div>
-                                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                                            <div class="w-full rounded-full bg-gray-200 dark:bg-gray-700">
                                                 @switch($item->status_aktivitas)
                                                     @case(1)
-                                                        <div class="bg-indigo-600 text-xs font-medium text-indigo-100 text-center p-0.5 leading-none rounded-full"
+                                                        <div class="rounded-full bg-indigo-600 p-0.5 text-center text-xs font-medium leading-none text-indigo-100"
                                                             style="width: {{ ($item->status_aktivitas / 3) * 100 }}%">
-                                                            {{ round(($item->status_aktivitas / 3) * 100) }} %</div>
+                                                            {{ round(($item->status_aktivitas / 3) * 100) }} %
+                                                        </div>
                                                     @break
 
                                                     @case(2)
-                                                        <div class="bg-indigo-600 text-xs font-medium text-indigo-100 text-center p-0.5 leading-none rounded-full"
+                                                        <div class="rounded-full bg-indigo-600 p-0.5 text-center text-xs font-medium leading-none text-indigo-100"
                                                             style="width: {{ ($item->status_aktivitas / 3) * 100 }}%">
-                                                            {{ round(($item->status_aktivitas / 3) * 100) }} %</div>
+                                                            {{ round(($item->status_aktivitas / 3) * 100) }} %
+                                                        </div>
                                                     @break
 
                                                     @case(3)
-                                                        <div class="bg-red-600 text-xs font-medium text-red-100 text-center p-0.5 leading-none rounded-full"
-                                                            style="width: 100%">100 %</div>
+                                                        <div class="rounded-full bg-red-600 p-0.5 text-center text-xs font-medium leading-none text-red-100"
+                                                            style="width: 100%">
+                                                            100 %
+                                                        </div>
                                                     @break
 
                                                     @case(4)
-                                                        <div class="bg-indigo-600 text-xs font-medium text-indigo-100 text-center p-0.5 leading-none rounded-full"
-                                                            style="width: 100%"> 100 %</div>
+                                                        <div class="rounded-full bg-indigo-600 p-0.5 text-center text-xs font-medium leading-none text-indigo-100"
+                                                            style="width: 100%">
+                                                            100 %
+                                                        </div>
                                                     @break
 
                                                     @default
-                                                        <div class="bg-indigo-600 text-xs font-medium text-indigo-100 text-center p-0.5 leading-none rounded-full"
-                                                            style="width: 0%"> 0%</div>
+                                                        <div class="rounded-full bg-indigo-600 p-0.5 text-center text-xs font-medium leading-none text-indigo-100"
+                                                            style="width: 0%">
+                                                            0%
+                                                        </div>
                                                 @endswitch
-
                                             </div>
                                         </div>
                                     </div>
@@ -84,18 +99,20 @@
                             </div>
                             @empty
                                 <div>Anda belum ada ajuan permohonan layanan</div>
-                                <div>Untuk membuat permohonan layanan Anda dapat mengklik tautan berikut <a
-                                        class="text-indigo-800 underline" href="{{ route('permohonan.index') }}">Buat
-                                        Permohonan</a></div>
+                                <div>
+                                    Untuk membuat permohonan layanan Anda dapat mengklik tautan berikut
+                                    <a class="text-indigo-800 underline" href="{{ route('permohonan.index') }}">Buat
+                                        Permohonan</a>
+                                </div>
                             @endforelse
                         </x-card>
+
+                    </div>
+                    <div class="w-1/3">
+                        Riwayat Permohonan
                     </div>
                 </section>
 
-                <section class="mb-8">
-                    <x-card>
-                    </x-card>
-                </section>
             </div>
         </div>
     </x-app-layout>
